2c5b8d1f9effed191211028000d23bc7e979f21a
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-12-27 19:46:58 +0100

    feat: refactor api client + jwt token handling

--- src/Model/API/BEditaClient.php
+++ src/Model/API/BEditaClient.php
@@ -14,16 +14,21 @@
 namespace App\Model\API;
 
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Uri;
 use Http\Adapter\Guzzle6\Client;
+use Psr\Http\Message\ResponseInterface;
 use WoohooLabs\Yang\JsonApi\Client\JsonApiClient;
-use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
 
 /**
  * BEdita4 API Client class
  */
 class BEditaClient
 {
+    /**
+     * @var ResponseInterface
+     *
+     */
+    private $response;
+
     /**
      * BEdita4 API base URL
      *
@@ -39,27 +44,110 @@ class BEditaClient
     private $apiKey = null;
 
     /**
+     * Default headers in request
+     *
+     * @var array
+     */
+    private $defaultHeaders = [
+        'Accept' => 'application/vnd.api+json',
+    ];
+
+    /**
+     * JWT Auth tokens
+     *
+     * @var array
+     */
+    private $tokens = [];
+
+    /**
      * JSON API BEdita4 client
      *
      * @var \WoohooLabs\Yang\JsonApi\Client\JsonApiClient
      */
-    private $apiClient = null;
-    
+    private $jsonApiClient = null;
+
     /**
-     * Setup main client options: API base URL and API KEY
-     * 
+     * Setup main client options:
+     *  - API base URL
+     *  - API KEY
+     *  - Auth tokens 'jwt' and 'renew' (optional)
+     *
      * @param string $apiUrl API base URL
      * @param string $apiKey API key
+     * @param array $tokens JWT Autorization tokens as associative array ['jwt' => '###', 'renew' => '###']
      * @return void
      */
-    public function __construct($apiUrl, $apiKey = null) 
+    public function __construct($apiUrl, $apiKey = null, array $tokens = [])
     {
         $this->apiBaseUrl = $apiUrl;
         $this->apiKey = $apiKey;
 
+        $this->defaultHeaders['X-Api-Key'] = $this->apiKey;
+        $this->setupTokens($tokens);
+
         // setup an asynchronous JSON API client
         $guzzleClient = Client::createWithConfig([]);
-        $this->apiClient = new JsonApiClient($guzzleClient);
+        $this->jsonApiClient = new JsonApiClient($guzzleClient);
+    }
+
+    /**
+     * Setup JWT access and refresh tokens.
+     *
+     * @param array $tokens JWT tokens as associative array ['jwt' => '###', 'renew' => '###']
+     * @return array Response in array format
+     */
+    protected function setupTokens(array $tokens)
+    {
+        $this->tokens = $tokens;
+        if (!empty($tokens['jwt'])) {
+            $this->defaultHeaders['Authorization'] = 'Bearer ' . $tokens['jwt'];
+        } else {
+            unset($this->defaultHeaders['Authorization']);
+        }
+    }
+
+    /**
+     * Get last HTTP response
+     *
+     * @return ResponseInterface Response PSR interface
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Get HTTP response status code
+     *
+     * @return int Status code.
+     */
+    public function getStatusCode()
+    {
+        return $this->response->getStatusCode();
+    }
+
+    /**
+     * Get response body serialized into a PHP array
+     *
+     * @return array Response body as PHP array.
+     */
+    public function getResponseBody()
+    {
+        return json_decode($this->response->getBody()->__toString(), true);
+    }
+
+    /**
+     * Classic authentication via POST /auth using username and password
+     *
+     * @param string $username
+     * @param string $password
+     * @return array Response in array format
+     */
+    public function authenticate($username, $password)
+    {
+        $body = json_encode(compact('username', 'password'));
+
+        return $this->post('/auth', $body, ['Content-Type' => 'application/json']);
     }
 
     /**
@@ -69,42 +157,76 @@ class BEditaClient
      * @param array $query Optional query string
      * @return array Response in array format
      */
-    public function get($path, $query = []) 
+    public function get($path, $query = [], $headers = [])
     {
-        $response = $this->sendRequest('GET', $path, $query);
-        $result = $response->document()->toArray();
-        // avoid missing `data` key even if it should be there... to investigate..  
-        if (empty($result['error']) && !isset($result['data'])) {
-            $result['data'] = [];
-        }
+        $this->sendRequest('GET', $path, $query, $headers);
 
-        return $result;
+        return $this->getResponseBody();
     }
 
     /**
-     * Send a generic JSON API request and retrieve response
+     * Send a POST request for creating resources or objects or other operations like /auth
+     *
+     * @param string $endpoint Endpoint URL path to invoke
+     * @param array $headers Custom request headers
+     * @return array Response in array format
+     */
+    public function post($path, $body, $headers = [])
+    {
+        $this->sendRequest('POST', $path, null, $headers, $body);
+
+        return $this->getResponseBody();
+    }
+
+    /**
+     * Send a generic JSON API request and retrieve response $this->response
      *
      * @param string $endpoint Endpoint URL path to invoke
      * @param array $query Optional query string
-     * @return \WoohooLabs\Yang\JsonApi\Response\JsonApiResponse JSON API Response object
+     * @param array $headers Custom request headers
+     * @param mixed $body Request body
+     * @param boolean $refresh In case of token expired response try a token refresh and repeat request. Default 'true'. Used to avoid loops.
+     * @return void
      */
-    protected function sendRequest($method, $path, $query = []) 
-    {        
+    protected function sendRequest($method, $path, $query = [], $headers = [], $body = null, $refresh = true)
+    {
         $uri = $this->apiBaseUrl . $path;
         if ($query) {
             $uri .= '?' . http_build_query($query);
         }
-        $request = new Request($method, $uri);
-        $request = $request
-            ->withHeader('Accept', 'application/vnd.api+json')
-            ->withHeader('X-Api-Key', $this->apiKey);
+        $headers = array_merge($this->defaultHeaders, $headers);
 
         // Send the request syncronously to retrieve the response
-        $psr7Response = $this->apiClient->sendRequest($request);
-        $response = new JsonApiResponse($psr7Response);
-
-        return $response;
+        $this->response = $this->jsonApiClient->sendRequest(new Request($method, $uri, $headers, $body));
+        // in case of 401 response with `be_token_expired` code refresh token and send request again
+        if ($refresh && $this->getStatusCode() === 401 &&
+            Hash::get($this->getResponseBody(), 'error.code') === 'be_token_expired') {
+            if ($this->refreshTokens()) {
+                return $this->sendRequest($method, $path, $query, $headers, $body, false);
+            }
+        }
     }
 
+    /**
+     * Send a refresh token request.
+     * On success `$this->token` data will be updated with new access and renew tokens.
+     *
+     * @return boolean True on success, false on failure
+     */
+    public function refreshTokens()
+    {
+        if (empty($this->tokens['renew'])) {
+            return false;
+        }
+        $headers = array_merge($this->defaultHeaders, ['Authorization' => 'Bearer ' . $tokens['renew']]);
+        $uri = $this->apiBaseUrl . '/auth';
+        $response = $this->jsonApiClient->sendRequest(new Request('POST', $uri, $headers));
+        $meta = Hash::get(json_decode($this->response->getBody()->__toString(), true), 'meta');
+        if (empty($meta['jwt'])) {
+            return false;
+        }
+        $this->setupTokens($meta);
 
+        return true;
+    }
 }
