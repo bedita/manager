e2b1f82ee6b7419abac4c7f124f1f4f4ba3a38d8
Dante Di Domenico <d.didomenico@channelweb.it>
2018-02-05 09:03:37 +0100

    [chore] fix: modules component test getModules fix

--- tests/TestCase/Controller/Component/ModulesComponentTest.php
+++ tests/TestCase/Controller/Component/ModulesComponentTest.php
@@ -243,7 +243,7 @@ class ModulesComponentTest extends TestCase
                     'supporto',
                 ],
                 [
-                    'resources' => [
+                    [
                         'name' => 'plugin',
                     ],
                 ],
