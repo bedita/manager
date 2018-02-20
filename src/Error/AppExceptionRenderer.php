03f54df745dc0417a61b9861a2244105b53be63d
Dante Di Domenico <d.didomenico@channelweb.it>
2018-02-20 18:14:18 +0100

    [chore] fix: header and class php doc

--- src/Error/AppExceptionRenderer.php
+++ src/Error/AppExceptionRenderer.php
@@ -1,10 +1,25 @@
 <?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2018 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
 
 namespace App\Error;
 
 use Cake\Error\ExceptionRenderer;
 use Exception;
 
+/**
+ * Custom exception renderer class.
+ * Handle with templates 500 and 400 (for status code < 500).
+ */
 class AppExceptionRenderer extends ExceptionRenderer
 {
     /**
