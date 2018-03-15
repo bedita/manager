7f5adba2d4bb75d9aa841365023b227905431c79
vallo87 <mazzai.valerio@googlemail.com>
2018-03-15 18:09:41 +0100

    feat: added json editor

---
+++ webroot/js/app/directives/jsoneditor.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * v-richeditor directive to activate ckeditor on element
+ *
+ */
+
+const jsonEditorOptions = {
+    "mode": "tree",
+    "modes": ["tree", "code"],
+    "history": true,
+    "search": true,
+    onChange: function () {
+        if (element) {
+            const json = element.jsonEditor.get();
+            element.value = JSON.stringify(json);
+        }
+    },
+};
+
+Vue.directive('jsoneditor', {
+    element: null,
+    /**
+     * When the bound element is inserted into the init CKeditor
+     *
+     * @param {Object} element DOM object
+     */
+    inserted (element) {
+        this.element = element;
+        const content = element.value;
+        try {
+            const json = JSON.parse(content);
+
+            if (json) {
+                element.style.display = "none";
+                let jsonEditor = document.createElement('div');
+                jsonEditor.className = "jsoneditor-container";
+                element.parentElement.insertBefore(jsonEditor, element);
+                element.jsonEditor = new JSONEditor(jsonEditor, jsonEditorOptions);
+                element.jsonEditor.set(json);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    },
+});
