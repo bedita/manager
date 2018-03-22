291f7939088aa803ad7d989eb3a7fd8a2076d531
stefanorosanelli <stefano.rosanelli@gmail.com>
2018-03-22 12:41:22 +0100

    fix: force GET /home reload in dashboard

--- src/Controller/Component/ModulesComponent.php
+++ src/Controller/Component/ModulesComponent.php
@@ -39,6 +39,7 @@ class ModulesComponent extends Component
     protected $_defaultConfig = [
         'apiClient' => null,
         'currentModuleName' => null,
+        'clearHomeCache' => false,
     ];
 
     /**
@@ -48,6 +49,10 @@ class ModulesComponent extends Component
      */
     public function beforeRender() : void
     {
+        if ($this->getConfig('clearHomeCache')) {
+            Cache::delete(sprintf('home_%d', $this->Auth->user('id')));
+        }
+
         $modules = $this->getModules();
         $project = $this->getProject();
 
