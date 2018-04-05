a3d5bb4253e28662e34b0bd727b8a6e819e9182f
vallo87 <mazzai.valerio@googlemail.com>
2018-04-05 18:59:46 +0200

    #37 refactor: vue components for relations

--- webroot/js/app/components/relation-view/relation-view.js
+++ webroot/js/app/components/relation-view/relation-view.js
@@ -7,14 +7,152 @@
  */
 
 Vue.component('relation-view', {
-    mixins: [ PaginatedRelationMixin ],
+    mixins: [ PaginatedContentMixin ],
 
     props: ['relationName', 'loadOnStart'],
 
-    created() {
-        if (this.loadOnStart) {
-            this.getRelatedObjects();
+    data() {
+        return {
+            method: 'relatedJson',          // define AppController method to be used
+            loading: false,
+            showRelationshipsPanel: false,
+
+            removedRelated: [],             // currently related objects to be removed
+            addedRelations: [],             // staged added objects to be saved
+            hideRelations: [],              // hide already added relations in relationships-view
+            relationsData: [],              // hidden field containing serialized json passed on form submit
         }
+    },
+
+    /**
+     * setup correct endpoint for PaginatedContentMixin.getPaginatedObjects()
+     *
+     * @return {void}
+     */
+    created() {
+        this.endpoint = `${this.method}/${this.relationName}`;
+
+        if (this.loadOnStart) {
+            this.loadRelatedObjects();
+        }
+    },
+
+    methods: {
+        /**
+         * call PaginatedContentMixin.getPaginatedObjects() method and handle loading
+         *
+         * @return {Boolean} response;
+         */
+        async loadRelatedObjects() {
+            this.loading = true;
+
+            const resp = await this.getPaginatedObjects();
+            this.loading = false;
+
+            return resp;
+        },
+
+        /**
+         * remove related object: adding it to removedRelated Array
+         *
+         * @param {Number} id
+         * @param {String} type
+         *
+         * @returns {void}
+         */
+        removeRelations(id, type) {
+            if (!this.containsId(this.removedRelated, id)) {
+                this.removedRelated.push({
+                    id,
+                    type,
+                });
+
+                this.relationsData = this.relationFormatterHelper(this.removedRelated);
+            }
+        },
+
+        // showMoreRelated() {
+        //     this.nextPage();
+        // },
+
+        /**
+         * remove element with matched id from staged relations
+         *
+         * @param {Number} id
+         */
+        removeAddedRelations(id) {
+            this.addedRelations = this.addedRelations.filter((rel) => rel.id !== id);
+        },
+
+        /**
+         * Show relationships-view and pass objects already related which need to be hidden
+         *
+         * @return {void}
+         */
+        showRelationshipsModal() {
+            // this.hideRelations is passed as prop to relationships-view
+            this.hideRelations = this.objects;
+            this.showRelationshipsPanel = true;
+        },
+
+        /**
+         * Event 'added-relations' callback
+         * retrieve last added relations from relationships-view
+         *
+         * @param {Array} relations
+         *
+         * @return {void}
+         */
+        appendRelations(relations) {
+            this.addedRelations = relations;
+        },
+
+        /**
+         * Event 'visibility-setter' callback
+         * set current view's relationships-view visibility from child view
+         *
+         * @param {Boolean} isVisible
+         *
+         * @return {void}
+         */
+        setRelationshipPanelVisibility(isVisible) {
+            this.showRelationshipsPanel = isVisible;
+        },
+
+        /**
+         * helper function: check if array relations has element with id -> id
+         *
+         * @param {Array} relations
+         * @param {Number} id
+         *
+         * @return {Boolean} true if id is in Array relations
+         */
+        containsId(relations, id) {
+            return relations.filter((rel) => rel.id === id).length;
+        },
+
+        /**
+         * helper function: convert array to string
+         *
+         * @param {Array} relations
+         *
+         * @return {String} string version of relations
+         */
+        relationFormatterHelper(relations) {
+            return JSON.stringify(relations);
+        },
+
+        /**
+         * helper function: build open view url
+         *
+         * @param {String} objectType
+         * @param {Number} objectId
+         *
+         * @return {String} url
+         */
+        buildViewUrl(objectType, objectId) {
+            return `${window.location.protocol}//${window.location.host}/${objectType}/view/${objectId}`;
+        },
     }
 
 });
---
+++ webroot/js/app/mixins/paginated-content.js
@@ -0,0 +1,58 @@
+/**
+ * Mixins: PaginatedContentMixin
+ *
+ *
+ */
+
+let PaginatedContentMixin = {
+    data() {
+        return {
+            objects: [],
+            endpoint: null,
+
+            pagination: {
+                page: 1,
+                page_size: 20,
+            },
+        }
+    },
+
+    methods: {
+        /**
+         * fetch objects based on this.endpoint value
+         *
+         * TO-DO pagination
+         *
+         * @return {Promise}
+         */
+        getPaginatedObjects() {
+            let baseUrl = window.location.href;
+            this.objects = [];
+
+            if (this.endpoint) {
+                const requestUrl = `${baseUrl}/${this.endpoint}`;
+                const options =  {
+                    credentials: 'same-origin',
+                    headers: {
+                        'accept': 'application/json',
+                    }
+                }
+
+                return fetch(requestUrl, options)
+                    .then((response) => response.json())
+                    .then((json) => {
+                        this.objects = json.data;
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } else {
+                return Promise.reject();
+            }
+        },
+    }
+}
+
+;(function(root) {
+    root.PaginatedContentMixin = PaginatedContentMixin;
+}(this));
