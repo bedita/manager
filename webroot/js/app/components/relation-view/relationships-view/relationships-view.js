d249015543f0eac42878a538a44afbfbb7f4da6d
vallo87 <mazzai.valerio@googlemail.com>
2018-04-20 17:15:38 +0200

    [minor] undefined var

--- webroot/js/app/components/relation-view/relationships-view/relationships-view.js
+++ webroot/js/app/components/relation-view/relationships-view/relationships-view.js
@@ -87,7 +87,7 @@ Vue.component('relationships-view', {
             }
             // avoid problem with vue rendering queue
             this.$nextTick( () => {
-                if (this.isVisible) {
+                if (this.isVisible && this.$refs.inputFilter) {
                     this.$refs.inputFilter.focus();
                 }
             });
