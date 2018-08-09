09a64e169a502f8d942cbef49fae78236f472846
vallo87 <mazzai.valerio@googlemail.com>
2018-08-09 15:50:10 +0200

    #155 feat: childrenView children position editing

---
+++ src/Template/Layout/js/app/components/children-view/children-view.js
@@ -0,0 +1,64 @@
+/**
+ *  Templates that uses this component (directly or indirectly):
+ *  Template/Elements/Form/children.twig
+ *
+ * <children-view> component used for ModulesPage -> View
+ *
+ * @prop {String} relationName name of the relation used by the PaginatiedContentMixin
+ * @prop {Boolean} loadOnStart load content on component init
+ *
+ */
+
+import Vue from 'vue';
+import RelationView from 'app/components/relation-view/relation-view';
+
+export default {
+    extends: RelationView,
+
+    data() {
+        return {
+            positions: {},
+        }
+    },
+
+    methods: {
+        updatePosition(related) {
+            const oldPosition = related.meta.relation.position;
+            const newPosition = this.positions[related.id] !== '' ? this.positions[related.id] : undefined;
+            if (newPosition !== oldPosition) {
+                // try to deep copy the object
+                try {
+                    const copy = JSON.parse(JSON.stringify(related));
+                    copy.meta.relation.position = newPosition;
+                    this.modifyRelation(copy);
+                } catch (exp) {
+                    // silent error
+                    console.error('[ChildreView -> updatePosition] something\'s wrong with the data');
+                }
+            } else {
+                this.removeModifiedRelations(related.id);
+            }
+        },
+
+                /**
+         * set modified relation to be saved
+         *
+         * @param {Object} related
+         */
+        modifyRelation(related) {
+            if (this.containsId(this.modifiedRelations, related.id)) {
+                // if object has been already modified we replace it within the modifiedRelations array
+                this.modifiedRelations = this.modifiedRelations.map((object) => {
+                    if (object.id === related.id) {
+                        return related;
+                    }
+                    return object;
+                });
+            } else {
+                // otherwise we add it to it
+                this.modifiedRelations.push(related);
+            }
+            this.setRelationsToSave();
+        },
+    }
+}
