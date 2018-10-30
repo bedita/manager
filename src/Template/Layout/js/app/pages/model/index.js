8b5041df70b1e7021174946fa27dddef435c43eb
vallo87 <mazzai.valerio@googlemail.com>
2018-10-30 17:40:40 +0100

    Merge

--- src/Template/Layout/js/app/pages/model/index.js
+++ src/Template/Layout/js/app/pages/model/index.js
@@ -27,6 +27,7 @@ export default {
      */
     data() {
         return {
+            propertyTypes: [],
             deletedPropertyTypes: [],
             savedPropertyTypes: [],
             newPropertyTypes: [],
