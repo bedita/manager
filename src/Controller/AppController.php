293455d663e80ff2ab17afeee34021c345ccd3c4
Dante Di Domenico <d.didomenico@channelweb.it>
2018-11-26 14:05:21 +0100

    fix: remove MessagesComponent and test

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -25,7 +25,6 @@ use Cake\Routing\Router;
  *
  * @property \App\Controller\Component\ModulesComponent $Modules
  * @property \App\Controller\Component\SchemaComponent $Schema
- * @property \App\Controller\Component\MessagesComponent $Messages
  */
 class AppController extends Controller
 {
@@ -66,7 +65,6 @@ class AppController extends Controller
             'currentModuleName' => $this->name,
         ]);
         $this->loadComponent('Schema');
-        $this->loadComponent('Messages');
     }
 
     /**
@@ -83,7 +81,9 @@ class AppController extends Controller
             if ($redirect !== '/') {
                 $route += compact('redirect');
             }
-            $this->redirect($route);
+            $this->Flash->error(__('You are not logged or your session has expired, please provide login credentials'));
+
+            return $this->redirect($route);
         }
         $this->setupOutputTimezone();
         $this->Messages->customize();
