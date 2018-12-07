f3c2e1950c639c9c83b2a28100db449be5b80cd3
vallo87 <mazzai.valerio@googlemail.com>
2018-12-07 16:59:51 +0100

    #204 feat: add configuration for bulk actions

--- src/Controller/Component/PropertiesComponent.php
+++ src/Controller/Component/PropertiesComponent.php
@@ -60,6 +60,9 @@ class PropertiesComponent extends Component
         'filter' => [
             'status',
         ],
+        'bulk' => [
+            'status',
+        ],
     ];
 
     /**
@@ -144,5 +147,17 @@ class PropertiesComponent extends Component
     public function filterList(string $type): array
     {
         return $this->getConfig(sprintf('Properties.%s.filter', $type), $this->defaultGroups['filter']);
+    }
+
+    /**
+     * List of filter to display in `filter` view
+     *
+     * @param string $type Object type name
+     *
+     * @return array
+     */
+    public function bulkList(string $type) : array
+    {
+        return $this->getConfig(sprintf('Properties.%s.bulk', $type), $this->defaultGroups['bulk']);
     }
 }
