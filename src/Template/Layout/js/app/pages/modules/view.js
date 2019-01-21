78dcf6234d617c44cc1d0de0956343cbe26243ab
vallo87 <mazzai.valerio@googlemail.com>
2019-01-21 10:39:29 +0100

    #251 feat: show donwload and view button for media across the app

--- src/Template/Layout/js/app/pages/modules/view.js
+++ src/Template/Layout/js/app/pages/modules/view.js
@@ -38,6 +38,46 @@ export default {
     methods: {
         toggleTabs() {
             return this.tabsOpen = !this.tabsOpen;
+        },
+
+        /**
+         * Force download using a syntetic element
+         *
+         * @param {*} blob
+         * @param {*} filename
+         */
+        forceDownload(blob, filename) {
+            let a = document.createElement('a');
+            a.download = filename;
+            a.href = blob;
+            a.click();
+        },
+
+        /**
+         * download a resource as a blob to avoid cors restrictions
+         *
+         * @param {string} url
+         * @param {string} filename
+         */
+        downloadResource(url, filename) {
+            if (!filename) {
+                filename = url.split('\\').pop().split('/').pop();
+            }
+
+            const options = {
+                headers: new Headers({
+                    'Origin': location.origin
+                }),
+                mode: 'cors'
+            }
+
+            fetch(url, options)
+                .then(response => response.blob())
+                .then(blob => {
+                    let blobUrl = window.URL.createObjectURL(blob);
+                    this.forceDownload(blobUrl, filename);
+                })
+                .catch(e => console.error(e));
         }
     }
 }
