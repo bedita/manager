37cdf4bbc9a60d0a64386485e3363d3f8fd12385
vallo87 <mazzai.valerio@googlemail.com>
2019-03-05 11:56:04 +0100

    #208 feat: basic FetchMixin using axios + axios dep

---
+++ src/Template/Layout/js/app/mixins/fetch.js
@@ -0,0 +1,53 @@
+/**
+ * Fetch mixin used to comunicate with cake controller's method
+ *
+ * TO-DO: handle token timeout
+ */
+
+import axios from 'axios';
+
+// private var to prevent tempering
+let privateAxios;
+
+export const FetchMixin = {
+    created() {
+        privateAxios = this.createCustomAxios();
+    },
+
+    computed: {
+        axios() {
+            return privateAxios;
+        }
+    },
+
+    methods: {
+        /**
+        * set up axios api
+        *
+        * @return {Object} custom axios instance
+        */
+        createCustomAxios() {
+            return axios.create({
+                baseURL: BEDITA.base,
+                timeout: 30000,
+                credentials: 'same-origin',
+                headers: {
+                    'accept': 'application/json',
+                    'content-type': 'multipart/form-data',
+                    'X-CSRF-Token': this.getCSFRToken(),
+                },
+
+                validateStatus: (status) => {
+                    return status >= 200 && status < 300; // default
+                },
+            });
+        },
+
+        /**
+         * get axios Factory
+         */
+        getAxios() {
+            return axios;
+        },
+    }
+}
