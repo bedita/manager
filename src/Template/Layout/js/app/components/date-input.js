85b19c01b4c52949d60ab371e040db8f66827182
vallo87 <mazzai.valerio@googlemail.com>
2019-03-14 17:47:28 +0100

    #282 feat: datepickr new dynamic loaded component with dynamic loaded vendors

---
+++ src/Template/Layout/js/app/components/date-input.js
@@ -0,0 +1,127 @@
+import 'flatpickr/dist/flatpickr.min.css';
+
+// fix import for locales
+
+import { Italian } from 'flatpickr/dist/l10n/it.js';
+import { French } from 'flatpickr/dist/l10n/fr.js';
+import { German } from 'flatpickr/dist/l10n/de.js';
+import { Spanish } from 'flatpickr/dist/l10n/es.js';
+import { Catalan } from 'flatpickr/dist/l10n/cat.js';
+import { Portuguese } from 'flatpickr/dist/l10n/pt.js';
+
+const LOCALES_AVAILABLE = {
+    it: Italian,
+    fr: French,
+    de: German,
+    es: Spanish,
+    cat: Catalan,
+    pt: Portuguese,
+}
+
+// date / time format (moment)
+const DATE_FORMAT = 'D/M/Y';
+const TIME_FORMAT = 'D/M/Y HH:mm';
+
+// default flatpickr options
+const FLATPICKR_OPTIONS = {
+    enableTime: false,
+    altInput: true,
+    dateFormat: 'Z', // ISO8601 for db
+    altFormat: DATE_FORMAT,
+    animate: false,
+    time_24hr: true,
+};
+
+// get loaded locale
+const LOCALE = BEDITA.locale.slice(0, 2);
+
+export default {
+    template: /* template */`
+    <div>
+        <slot></slot>
+    </div>
+    `,
+
+    props: {
+        el: {
+            type: HTMLInputElement,
+        },
+        attrs: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    async mounted() {
+        /** dynamic lib import */
+        let flatpickr;
+        let moment;
+
+        try {
+            flatpickr = await import(/* webpackChunkName: "flatpickr" */'flatpickr');
+            flatpickr = flatpickr.default;
+            moment = await import(/* webpackChunkName: "moment" */'moment');
+            moment = moment.default;
+        } catch (err) {
+            console.error(err);
+        }
+
+        // set locale
+        if (LOCALE !== 'en') {
+            moment.locale(LOCALE);
+            flatpickr.localize(LOCALES_AVAILABLE[LOCALE]);
+        }
+
+        let element = this.el;
+        let options = Object.assign({}, FLATPICKR_OPTIONS);
+
+        if (this.attrs.time) {
+            options.enableTime = this.time;
+            options.altFormat = TIME_FORMAT;
+        }
+
+        // custom format Date so we can show the current timezone
+        options.formatDate = (dateObj, format) => {
+            // this value goes to the hidden input which will be saved, needs to be a correct ISO8601
+            if (format === 'Z') {
+                return moment(dateObj).toISOString();
+            }
+            // or else timezone infos will be added to date string
+            let now = new Date();
+            let offset = now.getTimezoneOffset();
+            let tmz = offset / 60;
+            let sgn = tmz < 0 ? '+' : '-';
+            let formatUTC = `${format} (UTC${sgn}${Math.abs(tmz)})`;
+            return moment(dateObj).format(formatUTC);
+        }
+
+        try {
+            let datePicker = flatpickr(element, options);
+            // console.log(element);
+            element.dataset.originalValue = element.value;
+
+            let clearButton = document.createElement('span');
+            clearButton.classList.add('clear-button');
+            clearButton.innerHTML = '&times;';
+            clearButton.addEventListener('click', (ev) => {
+                ev.preventDefault()
+                ev.stopPropagation();
+                datePicker.clear();
+            });
+
+            element.parentElement.appendChild(clearButton);
+        } catch (err) {
+            console.error(err);
+        }
+    },
+
+    // methods: {
+    //     /**
+    //     *
+    //     * @emits Event#input textarea value
+    //     *
+    //     * @param {Event} event event object
+    //     */
+
+    // }
+};
--- src/Template/Layout/js/app/directives/datepicker.js
+++ src/Template/Layout/js/app/directives/datepicker.js
@@ -3,99 +3,28 @@
  *
  */
 
-import 'flatpickr/dist/flatpickr.min.css';
-
-import moment from 'moment/min/moment.min';
-import flatpickr from 'flatpickr';
-
-import { Italian } from 'flatpickr/dist/l10n/it.js';
-import { French } from 'flatpickr/dist/l10n/fr.js';
-import { German } from 'flatpickr/dist/l10n/de.js';
-import { Spanish } from 'flatpickr/dist/l10n/es.js';
-import { Catalan } from 'flatpickr/dist/l10n/cat.js';
-import { Portuguese } from 'flatpickr/dist/l10n/pt.js';
-
-const locales = {
-    it: Italian,
-    fr: French,
-    de: German,
-    es: Spanish,
-    cat: Catalan,
-    pt: Portuguese,
-}
-
-const dateTimePickerOptions = {
-    enableTime: true,
-    altInput: true,
-    dateFormat: 'Z', // ISO8601 for db
-    altFormat: 'D/M/Y HH:mm', // moment format / visible
-    animate: false,
-    time_24hr: true,
-};
-
-const datepickerOptions = {
-    enableTime: false,
-    altInput: true,
-    dateFormat: 'Z', // ISO8601 for db
-    altFormat: 'D/M/Y', // moment format / visible
-    animate: false,
-    time_24hr: true,
-};
-
-const trimmedLocale = BEDITA.locale.slice(0, 2);
-if (trimmedLocale !== 'en') {
-    moment.locale(trimmedLocale);
-    flatpickr.localize(locales[trimmedLocale]);
-}
-
 export default {
     install(Vue) {
         Vue.directive('datepicker', {
             /**
-             * create flatpicker instance when element is inserted
-             *
-             * @param {Object} element DOM object
-             */
-            inserted (element, dir, vueEl) {
-                let options = datepickerOptions;
-
-                if (vueEl.data && vueEl.data.attrs && vueEl.data.attrs.time === 'true') {
-                    options = dateTimePickerOptions;
-                }
-
-                // custom format Date so we can show the current timezone
-                options.formatDate = (dateObj, format) => {
-                    // this value goes to the hidden input which will be saved, needs to be a correct ISO8601
-                    if (format === 'Z') {
-                        return moment(dateObj).toISOString();
-                    }
-                    // or else timezone infos will be added to date string
-                    let now = new Date();
-                    let offset = now.getTimezoneOffset();
-                    let tmz = offset / 60;
-                    let sgn = tmz < 0 ? '+' : '-';
-                    let formatUTC = `${format} (UTC${sgn}${Math.abs(tmz)})`;
-                    return moment(dateObj).format(formatUTC);
-                }
-
-                try {
-                    let datePicker = flatpickr(element, options);
-                    element.dataset.originalValue = element.value;
-                    // element._flatpickr = datePicker;
-
-                    let clearButton = document.createElement('span');
-                    clearButton.classList.add('clear-button');
-                    clearButton.innerHTML = '&times;';
-                    clearButton.addEventListener('click', (ev) => {
-                        ev.preventDefault()
-                        ev.stopPropagation();
-                        datePicker.clear();
+            * create flatpicker instance when element is binded
+            *
+            * @param {Object} element DOM object
+            */
+            inserted(el, binding, vnode) {
+                const attrs = vnode.data && vnode.data.attrs;
+                import(/* webpackChunkName: "date-input" */'app/components/date-input')
+                    .then(module => module.default)
+                    .then((component) => {
+                        const Constructor = Vue.extend(component);
+                        const vm = new Constructor({
+                            propsData: {
+                                attrs,
+                                el,
+                            }
+                        });
+                        vm.$mount();
                     });
-
-                    element.parentElement.appendChild(clearButton);
-                } catch (err) {
-                    console.error(err);
-                }
             },
         })
     }
