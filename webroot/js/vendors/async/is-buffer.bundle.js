5a8027e3fdf19f76c00d6e2f2ddf068df5eb7741
vallo87 <mazzai.valerio@googlemail.com>
2019-04-29 18:08:10 +0200

    #299 chore: build

--- webroot/js/vendors/async/is-buffer.bundle.js
+++
@@ -1,8 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["vendors/async/is-buffer"],{"./node_modules/is-buffer/index.js":function(n,o){function e(n){return!!n.constructor&&"function"==typeof n.constructor.isBuffer&&n.constructor.isBuffer(n)}
-/*!
- * Determine if an object is a Buffer
- *
- * @author   Feross Aboukhadijeh <https://feross.org>
- * @license  MIT
- */
-n.exports=function(n){return null!=n&&(e(n)||function(n){return"function"==typeof n.readFloatLE&&"function"==typeof n.slice&&e(n.slice(0,0))}(n)||!!n._isBuffer)}}}]);
--- webroot/js/vendors/webpack.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["vendors/webpack"],{"./node_modules/webpack/buildin/global.js":function(n,o){var e;e=function(){return this}();try{e=e||Function("return this")()||(0,eval)("this")}catch(n){"object"==typeof window&&(e=window)}n.exports=e}}]);
