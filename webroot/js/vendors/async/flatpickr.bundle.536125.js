7ab707344875b497ced6250623dbbf1180f046e0
stefanorosanelli <stefano.rosanelli@gmail.com>
2019-05-22 17:28:47 +0200

    fix: update bundles

--- webroot/js/vendors/async/flatpickr.bundle.536125.js
+++ webroot/js/vendors/async/flatpickr.bundle.536125.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["vendors/async/flatpickr"],{32:function(e,n,t){
+(window.webpackJsonp=window.webpackJsonp||[]).push([["vendors/async/flatpickr"],{31:function(e,n,t){
 /* flatpickr v4.5.7, @license MIT */
 e.exports=function(){"use strict";
 /*! *****************************************************************************
