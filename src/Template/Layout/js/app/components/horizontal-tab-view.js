effe71190bca2ee7b28f8e61315b0cc95fd3246e
xho <xhoxho@gmail.com>
2019-05-22 15:46:40 +0200

    feat: js components horizontal tab view and file-upload + templates + media embed from URL WIP

---
+++ src/Template/Layout/js/app/components/horizontal-tab-view.js
@@ -0,0 +1,38 @@
+/**
+ * Templates that uses this component (directly or indirectly):
+ *  ...
+ *
+ * <horizontal-tab-view> component used for ModulesPage -> View
+ *
+ * Handle horizontal tabs
+ *
+ * @prop {Array} labels
+ * @prop {int} defaultActive index of the default active label in labels
+ *
+ */
+
+export default {
+    components: {
+        FormFileUpload: () => import(/* webpackChunkName: "form-file-upload" */'app/components/form-file-upload'),
+    },
+
+    props: {
+        labels: {
+            type: Array,
+            default: [],
+        },
+        defaultActive: {
+            type: Number,
+            default: 0,
+        },
+    },
+
+    data() {
+        return {
+            activeIndex: this.defaultActive,
+        }
+    },
+
+    methods: {
+    }
+}
