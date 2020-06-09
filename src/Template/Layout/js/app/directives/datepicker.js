a08cd44d8daef3259271a75a06c65daa133d5d1c
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2020-06-09 14:32:07 +0200

    #403 refactor: update calendar instance with directive

--- src/Template/Layout/js/app/directives/datepicker.js
+++ src/Template/Layout/js/app/directives/datepicker.js
@@ -8,17 +8,15 @@ export default {
     install(Vue) {
         Vue.directive('datepicker', {
             /**
-            * create flatpicker instance when element is binded
-            *
-            * @param {Object} element DOM object
-            */
+             * create flatpicker instance when element is binded
+             */
             inserted(el, binding, vnode) {
                 const attrs = vnode.data && vnode.data.attrs;
                 import(/* webpackChunkName: "date-input" */'app/components/date-input')
                     .then(module => module.default)
                     .then((component) => {
                         const Constructor = Vue.extend(component);
-                        const vm = new Constructor({
+                        const vm = el.vm = new Constructor({
                             propsData: {
                                 attrs,
                                 el,
@@ -26,6 +24,30 @@ export default {
                         });
                         vm.$mount();
                     });
+            },
+
+            /**
+             * update component value
+             */
+            componentUpdated(el, binding, vnode) {
+                if (!el.vm) {
+                    return;
+                }
+                if (vnode.data && vnode.data.domProps && vnode.data.domProps.value) {
+                    el.vm.setDate(new Date(vnode.data.domProps.value));
+                } else {
+                    el.vm.setDate(null);
+                }
+            },
+
+            /**
+             * destroy instance
+             */
+            unbind(el) {
+                if (el.vm) {
+                    el.vm.$destroy();
+                    delete el.vm;
+                }
             },
         })
     }
