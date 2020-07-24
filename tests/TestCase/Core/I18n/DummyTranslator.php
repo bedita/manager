78c204613da3fa4ed80fb5f26fc360dfad53b676
Dante Di Domenico <d.didomenico@channelweb.it>
2020-07-24 14:40:11 +0200

    feat: tests

---
+++ tests/TestCase/Core/I18n/DummyTranslator.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Test\TestCase\Core\I18n;
+
+use App\Core\I18n\TranslatorInterface;
+
+class DummyTranslator implements TranslatorInterface
+{
+    /**
+     * The engine options.
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * Setup translator engine.
+     *
+     * @param array $options The options
+     * @return void
+     */
+    public function setup(array $options = []): void
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * Translate an array of texts $texts from language source $from to language target $to
+     *
+     * @param array $texts The texts to translate
+     * @param string $from The source language
+     * @param string $to The target language
+     * @return string The translation in json format, i.e.
+     * {
+     *     "translation": [
+     *         "<translation of first text>",
+     *         "<translation of second text>",
+     *         [...]
+     *         "<translation of last text>"
+     *     ]
+     * }
+     */
+    public function translate($texts, string $from, string $to): string
+    {
+        $translation = [];
+        foreach ($texts as $text) {
+            $translation[] = sprintf('text: %s, from: %s, to: %s', $text, $from, $to);
+        }
+
+        return json_encode(compact('translation'));
+    }
+}
