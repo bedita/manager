c2409045eaaaa5e88abab03cb1dd02adeed9d59e
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2020-09-29 12:46:25 +0200

    styled form

--- src/Template/Layout/js/app/components/locations-view.js
+++ src/Template/Layout/js/app/components/locations-view.js
@@ -1,3 +1,5 @@
+import { t } from 'ttag';
+
 /**
  * Templates that uses this component (directly or indirectly):
  *  ...
@@ -10,17 +12,51 @@
  */
 export default {
     template: `<div class="locations">
-        <div v-for="location in locations.data">
-            <div class="location">
-                <: location.attributes.title :>
+        <div v-for="(location, key) in locations.data">
+            <div class="location is-flex">
+                <div class="order mr-1 p-1 has-background-white is-flex align-center has-text-black"> <: key + 1 :> </div>
+                <div class="location-form is-flex-column">
+                    <div class="is-flex">
+                        <div class="is-flex-column is-expanded">
+                            <label><: t('Title') :>
+                                <input type="text"/>
+                            </label>
+                        </div>
+                        <div class="is-flex-column is-expanded">
+                            <label><: t('Address') :>
+                                <input type="text"/>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="is-flex mt-1">
+                        <div class="is-flex-column is-expanded">
+                            <label><: t('Long Lat Coordinates') :>
+                                <input type="text"/>
+                            </label>
+                        </div>
+                        <div class="is-flex-column">
+                            <label> Zoom
+                                <input type="number" min="2" max="20"/>
+                            </label>
+                        </div>
+                        <div class="is-flex-column">
+                            <label> Pitch°
+                                <input type="number" min="0" max="60"/>
+                            </label>
+                        </div>
+                        <div class="is-flex-column">
+                            <label> Bearing°
+                                <input type="number" min="-180" max="180"/>
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>`,
 
     props: {
-        object: {
-            type: Object,
-        },
+        object: Object,
     },
 
     data() {
