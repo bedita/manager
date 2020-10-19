27d164960063fc1d769aa4a0840e7c73dde87f47
Dante Di Domenico <d.didomenico@channelweb.it>
2020-10-19 16:07:54 +0200

    feat: reset password

---
+++ src/Controller/PasswordController.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2020 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace App\Controller;
+
+use App\Application;
+use BEdita\SDK\BEditaClientException;
+use Cake\Event\Event;
+use Cake\Http\Response;
+use Cake\Utility\Hash;
+
+/**
+ * Perform password reset and change.
+ */
+class PasswordController extends AppController
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->Auth->allow(['reset', 'change']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function beforeFilter(Event $event): ?Response
+    {
+        // if authenticated, redirect to dashboard
+        $tokens = $this->Auth->user('tokens');
+        if (!empty($tokens)) {
+            return $this->redirect('/dashboard');
+        }
+
+        return null;
+    }
+
+    /**
+     * Password reset.
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function reset(): ?Response
+    {
+        $this->request->allowMethod(['get', 'post']);
+
+        if ($this->request->is('get')) {
+            return null;
+        }
+
+        $data = [
+            'contact' => $this->request->getData('email'),
+            'change_url' => Router::url(['_name' => 'password:change'], true),
+        ];
+        try {
+            $result = $this->apiClient->post(
+                '/auth/change',
+                json_encode($data),
+                ['Content-Type' => 'application/json']
+            );
+            $this->set(compact('result'));
+        } catch (BEditaClientException $ex) {
+            $this->Flash->error(__($ex->getMessage()));
+
+            return $this->redirect(['_name' => 'password:reset']);
+        }
+        $this->viewBuilder()->setTemplate('request_sent');
+
+        return null;
+    }
+
+    /**
+     * Password change action.
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function change(): ?Response
+    {
+        $this->request->allowMethod(['get', 'post']);
+
+        if ($this->request->is('get')) {
+            $this->set('uuid', $this->request->getQuery('uuid'));
+
+            return null;
+        }
+
+        $data = [
+            'uuid' => $this->request->getData('uuid'),
+            'password' => $this->request->getData('password'),
+            'login' => true,
+        ];
+        try {
+            $result = $this->apiClient->patch(
+                '/auth/change',
+                json_encode($data),
+                ['Content-Type' => 'application/json']
+            );
+        } catch (BEditaClientException $ex) {
+            $this->Flash->error(__($ex->getMessage()));
+            $this->redirect(['_name' => 'password:reset']);
+        }
+
+        $tokens = $result['meta'];
+        $user = new \ArrayObject($result['data']
+            + compact('tokens')
+            + Hash::combine($result, 'included.{n}.attributes.name', 'included.{n}.id', 'included.{n}.type'));
+        $this->Authentication->setIdentity($user);
+        $this->set(compact('result'));
+
+        return $this->redirect(['_name' => 'dashboard']);
+    }
+}
