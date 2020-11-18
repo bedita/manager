a11e7cf858178704a4ad27709dec46313569d39f
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2020-11-18 10:55:20 +0100

    feat: implement placeholder in CKEditor

--- src/Template/Layout/js/app/lib/plugins/placeholders.js
+++ src/Template/Layout/js/app/lib/plugins/placeholders.js
@@ -1,5 +1,6 @@
 import { PanelEvents } from 'app/components/panel-view';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 
@@ -29,58 +30,93 @@ export default class InsertPlaceholders extends Plugin {
                     },
                 });
 
-                // @TODO get list
+                let onSave = (objects) => {
+                    PanelEvents.closePanel();
+                    objects.forEach((data) => {
+                        editor.model.change((writer) => {
+                            let element = writer.createElement('placeholder', {
+                                id: data.id,
+                                type: data.type,
+                                params: {},
+                                data,
+                            });
 
-                editor.model.change((writer) => {
-                    let data = {};
-                    let element = writer.createElement('placeholder', {
-                        id: data.id,
-                        type: data.type,
-                        params: {},
-                        data,
+                            editor.model.insertContent(element, editor.model.document.selection);
+                        });
                     });
+                };
 
-                    editor.model.insertContent(element, editor.model.document.selection);
-                    console.log(editor.model);
-                });
+                let onClose = () => {
+                    PanelEvents.stop('relations-add:save', this, onSave);
+                    PanelEvents.stop('panel:close', this, onClose);
+                };
+
+                PanelEvents.listen('relations-add:save', this, onSave);
+                PanelEvents.listen('panel:close', this, onClose);
             });
 
             // register the placeholder schema to the ckeditor model
             schema.register('placeholder', {
-                // The placeholder will act as an inline node:
+                allowWhere: '$text',
                 isInline: true,
-
-                // The inline widget is self-contained so it cannot be split by the caret and it can be selected:
                 isObject: true,
+                allowAttributes: ['id', 'type', 'params', 'data'],
             });
 
             // convert the placeholder model to ckeditor output
-            conversion.for('upcast').elementToElement( {
-                view: {
-                    name: 'div',
-                    classes: 'bedita-placeholder',
-                },
-                model: (viewElement, { writer }) => {
-                    console.log('VIEW ELEMENT', viewElement);
-                    let fragment = upcastWriter.createDocumentFragment(viewElement.getChildren());
-                    let comment = htmlProcessor.toData(fragment);
-                    console.log('DATA', viewElement);
+            // conversion.for('upcast').elementToElement( {
+            //     view: {
+            //         name: 'span',
+            //         classes: ['be-placeholder'],
+            //     },
+            //     model: (viewElement, { writer }) => {
+            //         console.log('VIEW ELEMENT', viewElement);
+            //         let fragment = writer.createDocumentFragment(viewElement.getChildren());
+            //         let comment = htmlProcessor.toData(fragment);
+            //         console.log('DATA', viewElement);
 
-                    return writer.createElement('placeholder', {
-                        id: comment,
-                        type: comment,
-                        params: comment,
-                    });
-                }
+            //         return writer.createElement('placeholder', {
+            //             id: comment,
+            //             type: comment,
+            //             params: comment,
+            //         });
+            //     }
+            // });
+
+            conversion.for('editingDowncast').elementToElement( {
+                model: 'placeholder',
+                view: (modelItem, { writer }) => createPlaceholderView(modelItem, writer),
             });
 
             // convert html to ckeditor placeholder model
             conversion.for('dataDowncast').elementToElement( {
                 model: 'placeholder',
-                view: (modelElement, { writer }) => writer.createRawElement('div', { class: 'bedita-placeholder' }, (domElement) => {
-                    domElement.innerHTML = `<!-- BEPLACEHOLDER ${domElement.dataset.id} -->`;
-                }),
+                view: (modelItem, { writer }) => createPlaceholderView(modelItem, writer),
             });
+
+            function createPlaceholderView(modelItem, writer) {
+                let id = modelItem.getAttribute('id');
+                let type = modelItem.getAttribute('type');
+                let data = modelItem.getAttribute('data');
+                let params = modelItem.getAttribute('params') || {};
+                let placeholderView = writer.createRawElement('span', { class: 'be-placeholder' }, (/** @type {HTMLElement} */ dom) => {
+                    dom.innerHTML = `<!-- BE-PLACEHOLDER.${id}.${btoa(params)} -->`;
+                    let root = dom.shadowRoot || dom.attachShadow({
+                        mode: 'open',
+                    });
+
+                    switch (type) {
+                        case 'images':
+                            root.innerHTML = `<img src="${data.attributes.uri}" alt="${data.attributes.title}" />`;
+                            break;
+                        default:
+                            root.innerHTML = `<iframe src="${data.attributes.uri}"></iframe>`;
+                            break;
+                    }
+                });
+
+                return placeholderView;
+            }
 
             return view;
         });
