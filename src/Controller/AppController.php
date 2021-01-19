964bf2df52828f6ae391f7254cd1f885ed400d14
Dante Di Domenico <dante@didoda.com>
2021-01-19 17:05:15 +0100

    fix: less code

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -377,19 +377,11 @@ class AppController extends Controller
             return $this->redirect((string)$this->request->getUri()->withQuery(''));
         }
 
-        // _search === 1 => request from button Search
-        if ($this->request->getQuery('_search') === '1') {
-            $session->delete($sessionKey);
-            $q = $this->request->getQueryParams();
-            unset($q['_search']);
-            $query = http_build_query($q, '', '&', PHP_QUERY_RFC3986);
-
-            return $this->redirect((string)$this->request->getUri()->withQuery($query));
-        }
-
         // write request query parameters (if any) in session
-        if (!empty($this->request->getQueryParams())) {
-            $session->write($sessionKey, $this->request->getQueryParams());
+        $params = $this->request->getQueryParams();
+        if (!empty($params)) {
+            unset($params['_search']);
+            $session->write($sessionKey, $params);
 
             return null;
         }
