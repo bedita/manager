e1fe5fd61283b4289466866c14aa96b77c7a9848
Dante Di Domenico <dante@didoda.com>
2021-02-01 21:51:35 +0100

    chore test

--- webpack-gettext-loader.js
+++ webpack-gettext-loader.js
@@ -2,7 +2,7 @@ const po = require('gettext-parser').po;
 
 /**
  * @see https://github.com/cah4a/po-gettext-loader
- * Like the project above,  but with updated dependencies. //
+ * Like the project above,  but with updated dependencies.
  */
 module.exports = function(source) {
     this.cacheable();
