9d969231923fa5e7fec22b97e1f4bf93afa4660e
dante di domenico <dante@didoda.com>
2021-08-18 15:41:59 +0200

    feat: courtesy page on UnderWork conf

---
+++ src/Controller/CourtesyPageController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2021 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace App\Controller;
+
+use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Http\Response;
+
+/**
+ * CourtesyPage Controller
+ */
+class CourtesyPageController extends Controller
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index(): ?Response
+    {
+        $message = Configure::read('UnderWork.message');
+        $this->set(compact('message'));
+
+        return null;
+    }
+}
