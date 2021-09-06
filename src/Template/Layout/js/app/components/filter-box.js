f62e62c84528ede8a2d8f00dbd13b54716456342
dante di domenico <dante@didoda.com>
2021-09-06 14:54:49 +0200

    feat: i18n updates

--- src/Template/Layout/js/app/components/filter-box.js
+++ src/Template/Layout/js/app/components/filter-box.js
@@ -169,7 +169,7 @@ export default {
             }
             const label = this.ucfirst(this.objectsLabel);
 
-            return t`${label}`;
+            return label;
         },
 
         /**
