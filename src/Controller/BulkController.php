0ca8dc5310e3a1ac59e58c73d164ef3113f87fa2
dante di domenico <dante@didoda.com>
2021-09-28 17:49:09 +0200

    fix: phpcs

--- src/Controller/BulkController.php
+++ src/Controller/BulkController.php
@@ -163,7 +163,10 @@ class BulkController extends AppController
                 $objectCategories = (array)Hash::extract($object, 'data.attributes.categories.{n}.name');
                 $this->categories = !empty($objectCategories) ? array_unique(array_merge($this->categories, $objectCategories)) : $this->categories;
                 $payload = compact('id');
-                $payload['categories'] = array_map(function ($category) { return ['name' => $category]; }, $this->categories);
+                $payload['categories'] = array_map(function ($category) {
+
+                    return ['name' => $category];
+                }, $this->categories);
                 $this->apiClient->save($this->objectType, $payload);
             } catch (BEditaClientException $e) {
                 $this->errors[] = ['id' => $id, 'message' => $e->getAttributes()];
