0313a9a05fd253a9c8436c559dfb9b916183feda
dante di domenico <dante@didoda.com>
2021-10-01 10:29:02 +0200

    feat: refactor fetchFolders

--- src/Template/Layout/js/app/components/folder-picker/folder-picker.js
+++ src/Template/Layout/js/app/components/folder-picker/folder-picker.js
@@ -41,24 +41,31 @@ export default {
 
     methods: {
         async fetchFolders(parent) {
+            const pageSize = 100;
             const folders = [];
             let filter = !parent ? 'filter[roots]' : `filter[parent]=${parent.id}`;
-            let page = 1;
-            let done = false;
-            do {
-                let response = await fetch(`${API_URL}api/folders?${filter}&page=${page}&page_size=100`, API_OPTIONS);
-                let json = await response.json();
-                if (json.data) {
-                    const newFolders = json.data.map((folder) => ({id: folder.id, label: folder.attributes.title, children: null}));
-                    folders.push(...newFolders);
-                }
-                done = (!json.meta || !json.meta.pagination || json.meta.pagination.page_count === json.meta.pagination.page);
-                if (!done) {
-                    page = json.meta.pagination.page + 1;
-                }
-            } while (!done);
+            const firstResponse = await fetch(`${API_URL}api/folders?${filter}&page=1&page_size=${pageSize}`, API_OPTIONS);
+            const pageCount = await this.updateFolders(firstResponse, folders);
+            const deferred = [];
+            for (let page = 2; page <= pageCount; page++) {
+                deferred.push(fetch(`${API_URL}api/folders?${filter}&page=${page}&page_size=${pageSize}`, API_OPTIONS));
+            }
+            const responses = await Promise.all(deferred);
+            responses.forEach(async (response) => {
+                await this.updateFolders(response, folders);
+            });
 
             return folders;
+        },
+
+        async updateFolders(response, folders) {
+            let json = await response.json();
+            if (json.data) {
+                const newFolders = json.data.map((folder) => ({id: folder.id, label: folder.attributes.title, children: null}));
+                folders.push(...newFolders);
+            }
+
+            return json.meta.pagination.page_count;
         },
 
         async loadOptions({ action, parentNode, callback }) {
