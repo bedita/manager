cd53e1f520a6bce52a3413ea6b64486cbb61a4a8
dante di domenico <dante@didoda.com>
2021-11-10 14:14:46 +0100

    feat: confirm on delete

---
+++ src/Template/Layout/js/app/pages/admin/index.js
@@ -0,0 +1,23 @@
+/**
+ * Templates that uses this component (directly or indirectly)
+ *  Template/Pages/Admin/../*.twig
+ *
+ * <admin-index> component used for Admin index pages
+ */
+
+import { confirm } from 'app/components/dialog/dialog';
+import { t } from 'ttag';
+
+export default {
+    components: {
+        Secret: () => import(/* webpackChunkName: "secret" */'app/components/secret/secret'),
+    },
+
+    methods: {
+        trash(e) {
+            const message = t`Remove item. Are you sure?`;
+            const form = document.getElementById(e.target.getAttribute('form'));
+            confirm(message, t`yes, proceed`, form.submit.bind(form));
+        },
+    }
+};
