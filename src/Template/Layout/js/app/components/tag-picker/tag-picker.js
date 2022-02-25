9f9ea367196ac447ab83c3c307dfedb03a5316c4
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2022-02-25 15:41:33 +0100

    feat: input to add new tag

--- src/Template/Layout/js/app/components/tag-picker/tag-picker.js
+++ src/Template/Layout/js/app/components/tag-picker/tag-picker.js
@@ -19,7 +19,7 @@ export default {
         <div class="tag-picker">
             <label v-if="label" :for="id"><: label :></label>
             <Treeselect
-                placeholder
+                placeholder="Cerca tag esistenti"
                 :options="tagsOptions"
                 :async="true"
                 :clearable="false"
@@ -30,6 +30,13 @@ export default {
                 v-model="selectedIds"
                 @input="onChange"
             />
+            <div class="new-tag">
+                <label for="new-tag">Crea un nuovo tag</label>
+                <div class="input-container">
+                    <input type="text" id="new-tag" :value="value" name="new-tag" @input="update($event.target.value)" />
+                    <button @click.prevent="addNewTag">Aggiungi</button>
+                </div>
+            </div>
             <input type="hidden" :id="id" name="tags" :value="selectedIds" />
         </div>
     `,
@@ -43,6 +50,7 @@ export default {
 
     data() {
         return {
+            value: '',
             tagsOptions: [],
             selectedIds: [],
         };
@@ -56,6 +64,16 @@ export default {
     },
 
     methods: {
+        update(str) {
+            this.value = str;
+        },
+        addNewTag() {
+            // TODO
+            // - chiamata API per aggiungere il tag
+            // - svuotare l'input
+            // - aggiungere il tag fra quelli che ha l'oggetto
+            // - refresh lista dei tag esistenti
+        },
         onChange() {
             const selectedTags = this.initialTags.filter((tag) => this.selectedIds.includes(tag.id));
             this.$emit('change', selectedTags);
