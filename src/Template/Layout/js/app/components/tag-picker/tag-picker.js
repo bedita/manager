fa91dd533a33842818edc6359383902acf521717
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2022-03-01 10:13:49 +0100

    feat: add/remove/fetch tags on tag picker now working

--- src/Template/Layout/js/app/components/tag-picker/tag-picker.js
+++ src/Template/Layout/js/app/components/tag-picker/tag-picker.js
@@ -16,7 +16,7 @@ export default {
 
     template: `
         <div class="tag-picker">
-            <label v-if="label" :for="id"><: label :></label>
+            <label v-if="label"><: label :></label>
             <Treeselect
                 placeholder="Cerca tag esistenti"
                 :options="tagsOptions"
@@ -26,22 +26,24 @@ export default {
                 :disabled="disabled"
                 :disable-branch-nodes="true"
                 :multiple="true"
-                v-model="selectedIds"
-                @input="onChange"
+                value-format="object"
+                v-model="selectedTags"
+                @select="onAdd"
+                @deselect="onRemove"
             />
             <div class="new-tag">
-                <label for="new-tag">Crea un nuovo tag</label>
+                <label for="new-tag">Aggiungi un nuovo tag</label>
                 <div class="input-container">
-                    <input type="text" id="new-tag" :value="value" name="new-tag" @input="update($event.target.value)" />
+                    <input type="text" :value="text" id="new-tag" @input="update($event.target.value)" />
                     <button @click.prevent="addNewTag">Aggiungi</button>
                 </div>
             </div>
-            <input type="hidden" :id="id" name="tags" :value="selectedIds" />
+            <input type="hidden" name="tags" :value="modifiedTags" />
+            <input type="hidden" name="_types[tags]" value="json" />
         </div>
     `,
 
     props: {
-        id: String,
         disabled: Boolean,
         label: String,
         initialTags: Array,
@@ -49,64 +51,52 @@ export default {
 
     data() {
         return {
-            value: '',
+            selectedTags: [],
+            modifiedTags: null,
             tagsOptions: [],
-            selectedIds: [],
+            text: '',
         };
     },
 
     mounted() {
-        this.selectedIds = this.initialTags?.map((tag) => tag.id); // <= qui da sistemare | non si ha id
-        this.tagsOptions = this.initialTags?.map((tag) => tag.label) || []; // <= qui da sistemare
+        this.selectedTags = this.initialTags?.map((tag) => ({ id: tag.name, label: tag.label }));
+        this.parseBeforeSave();
     },
 
     methods: {
+        parseBeforeSave() {
+            this.modifiedTags = JSON.stringify(this.selectedTags.map((tag) => ({ name : tag.id, label: tag.label })));
+        },
+        onAdd(tag) {
+            this.selectedTags.push({
+                'id': tag.id,
+                'label': tag.label,
+            });
+            this.parseBeforeSave();
+        },
+        onRemove(tag) {
+            const index = this.selectedTags.findIndex((t) => t.id == tag.id);
+            this.selectedTags.splice(index, 1);
+            this.parseBeforeSave();
+        },
         update(str) {
-            this.value = str;
+            this.text = str;
         },
         addNewTag() {
-            // TODO
-            // - aggiungere il valore alla lista dei tag da salvare (it was: chiamata API per aggiungere il tag)
-            // - svuotare l'input
-            // - aggiungere il tag fra quelli che ha l'oggetto
-            // - refresh lista dei tag esistenti
-            // nota: il salvataggio è per tutto l'oggetto, con payload tipo
-//             PATCH /galleries/84862
-//             {
-//                 "data": {
-//                     "id": "84862",
-//                     "type": "galleries",
-//                     "attributes": {
-//                         "tags": [
-//                         	{
-//                         		"name": "abitare"
-//                         	},
-//                         	{
-//                         		"name": "abitare nuovo che non esiste già"
-//                         	},
-//                         	{
-//                         		"name": "tag nuovo a caso",
-//                                 "label": "TAG NUOVO A CASO"
-//                         	}
-//                         ]
-//                     }
-//                 }
-//             }
+            this.selectedTags.push({
+                'id': this.text,
+                'label': this.text,
+            });
+            this.text = '';
+            this.parseBeforeSave();
         },
-        onChange() {
-            const selectedTags = this.initialTags.filter((tag) => this.selectedIds.includes(tag.id));
-            this.$emit('change', selectedTags);
-        },
+
         async fetchTags({ action, searchQuery, callback }) {
             if (action !== ASYNC_SEARCH || searchQuery?.length < QUERY_MIN_LENGTH) {
-                return;
+                return callback(null, []);
             }
 
-            // TODO: da rivedere le righe seguenti, si può semplificare: non serve chiamata api.
-            // l'utente inserisce quello che gli pare, il sistema salva:
-            // se è nuovo, salva come nuovo, altrimenti usa tag esistente
-            // del resto "/api/model/tags" non è accessibile a tutti gli utenti, per cui questa parte non funzionerebbe in alcuni casi
-            const res = await fetch(`${BEDITA.base}/api/model/tags?filter[query]=${searchQuery}&page_size=100`, API_OPTIONS);
+            const res = await fetch(`${BEDITA.base}/api/model/tags?filter[query]=${searchQuery}&page_size=30`, API_OPTIONS);
             const json = await res.json();
             const tags = [...(json.data || [])];
 
