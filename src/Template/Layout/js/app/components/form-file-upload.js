13dd3e6a73b25688a8e4399116242f78d61dc95c
Dante Di Domenico <dante@didoda.com>
2022-03-23 08:47:31 +0100

    feat: check file size on upload (#732)

--- src/Template/Layout/js/app/components/form-file-upload.js
+++ src/Template/Layout/js/app/components/form-file-upload.js
@@ -11,6 +11,9 @@
  *
  */
 
+import { warning } from 'app/components/dialog/dialog';
+import { t } from 'ttag';
+
 export default {
     props: {
     },
@@ -26,6 +29,17 @@ export default {
 
     methods: {
         onFileChanged(e) {
+            const fileSize = Math.round(e.target.files[0].size);
+            const maxFileSize = BEDITA.maxFileSize;
+            if (fileSize >= maxFileSize) {
+                this.fileName = '';
+                const fileSizeMb = Math.round(fileSize / 1024 / 1024);
+                const maxFileSizeMb = Math.round(BEDITA.maxFileSize / 1024 / 1024);
+                const message = t`File too big (${fileSizeMb} MB), please select a file less than max file size (${maxFileSizeMb} MB)`;
+                warning(message);
+
+                return;
+            }
             this.fileName = e.target.files[0].name;
         },
 
