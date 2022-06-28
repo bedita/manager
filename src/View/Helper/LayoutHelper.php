2c811bde73e8803da94d7370192c65a1abd51506
dante di domenico <dante@didoda.com>
2022-06-28 09:15:39 +0200

    chore fix: ui

--- src/View/Helper/LayoutHelper.php
+++ src/View/Helper/LayoutHelper.php
@@ -136,6 +136,7 @@ class LayoutHelper extends Helper
             'Relations' => 'has-background-black',
             'PropertyTypes' => 'has-background-black',
             'Categories' => 'has-background-black',
+            'Appearence' => 'has-background-black',
             'Applications' => 'has-background-black',
             'AsyncJobs' => 'has-background-black',
             'Config' => 'has-background-black',
