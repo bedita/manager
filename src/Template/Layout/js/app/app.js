91a0528245d94514c8a6c068bc3bc43288f9a9e2
dante di domenico <dante@didoda.com>
2022-07-06 13:14:34 +0200

    feat: model object type properties hide

--- src/Template/Layout/js/app/app.js
+++ src/Template/Layout/js/app/app.js
@@ -39,6 +39,7 @@ const _vueInstance = new Vue({
         TreeView: () => import(/* webpackChunkName: "tree-view" */'app/components/tree-view/tree-view'),
         ModulesIndex: () => import(/* webpackChunkName: "modules-index" */'app/pages/modules/index'),
         ModulesView: () => import(/* webpackChunkName: "modules-view" */'app/pages/modules/view'),
+        ObjectProperty: () => import(/* webpackChunkName: "object-property" */'app/components/object-property/object-property'),
         ObjectTypesList: () => import(/* webpackChunkName: "object-types-list" */'app/components/object-types-list/object-types-list'),
         TrashIndex: () => import(/* webpackChunkName: "trash-index" */'app/pages/trash/index'),
         TrashView: () => import(/* webpackChunkName: "trash-view" */'app/pages/trash/view'),
---
+++ src/Template/Layout/js/app/components/object-property/object-property.js
@@ -0,0 +1,94 @@
+import { t } from 'ttag';
+
+export default {
+    template: `<div :class="boxClass()">
+        <div class="columns">
+            <div class="column">
+                <span :class="tagClass()"><: prop.attributes.name :></span>
+                <p>${t`Label`}: <: prop.attributes.label || '-' :></p>
+                <p>${t`Type`}: <: prop.attributes.property_type_name :></p>
+                <p>${t`Hidden`}:
+                    <span v-if="hidden">${t`Yes`}</span>
+                    <span v-if="!hidden">${t`No`}</span>
+                </p>
+                <p v-if="type === 'inherited'">
+                    ${t`Inherited from`}:
+                    <: prop.attributes.object_type_name :>
+                </p>
+                <p v-if="prop.attributes.description">&nbsp;</p>
+                <p v-if="prop.attributes.description"><: prop.attributes.description :></p>
+            </div>
+            <div v-if="!(prop.attributes.name in nobuttonsfor)" class="column is-narrow">
+                <div class="buttons-container">
+                    <button v-if="type === 'custom'" class="button is-expanded">${t`Delete`}</button>
+                    <button v-if="hidden" @click.prevent="toggle(false)" class="button is-expanded">${t`Show`}</button>
+                    <button v-if="!hidden" @click.prevent="toggle(true)" class="button is-expanded">${t`Hide`}</button>
+                </div>
+            </div>
+        </div>
+    </div>`,
+
+    props: {
+        prop: [],
+        nobuttonsfor: [],
+        type: '',
+        ishidden: false,
+    },
+
+    data() {
+        return {
+            hidden: false,
+        };
+    },
+
+    mounted() {
+        this.$nextTick(() => {
+            this.hidden = this.ishidden || false;
+        });
+    },
+
+    methods: {
+        boxClass() {
+            if (this.type === 'custom') {
+                return 'box pb-05 has-background-info has-text-white';
+            }
+            if (this.type === 'inherited') {
+                return 'box pb-05 has-background-gray-800 has-text-white';
+            }
+
+            return 'box pb-05 has-background-black has-text-white';
+        },
+        tagClass() {
+            if (this.type === 'custom') {
+                return 'tag is-dark';
+            }
+            if (this.type === 'inherited') {
+                return `tag has-background-module-${this.prop.attributes.object_type_name}`;
+            }
+
+            return 'tag';
+        },
+        toggle(val) {
+            this.hidden = val;
+            let hiddenProperties = JSON.parse(document.getElementById('hidden').value || '[]') || [];
+            const index = hiddenProperties.indexOf(this.prop.attributes.name);
+            if (index >= 0 && !this.hidden) {
+                // remove property from hidden properties
+                hiddenProperties.splice(index, 1);
+                const newVal = JSON.stringify(hiddenProperties);
+                document.getElementById('hidden').value = newVal;
+                document.getElementById('hidden').setAttribute('data-original-value', newVal);
+
+                return;
+            }
+            if (index < 0 && this.hidden) {
+                // add property to hidden properties
+                hiddenProperties.push(this.prop.attributes.name);
+                hiddenProperties.sort();
+                const newVal = JSON.stringify(hiddenProperties);
+                document.getElementById('hidden').value = newVal;
+                document.getElementById('hidden').setAttribute('data-original-value', newVal);
+            }
+        },
+    },
+}
