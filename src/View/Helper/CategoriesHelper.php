0c0f975a41cc101be440bfd132e3ebf32ce237ca
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2022-12-17 11:19:16 +0100

    fix: only enable children in tree

--- src/View/Helper/CategoriesHelper.php
+++ src/View/Helper/CategoriesHelper.php
@@ -112,7 +112,9 @@ class CategoriesHelper extends Helper
             return $controlOptions;
         }
         foreach ($node['children'] as $key => $child) {
-            $controlOptions['options'][$key] = ['value' => $child['name'], 'text' => $child['label'] = $child['label'] ?: $child['name']];
+            if ($child['enabled']) {
+                $controlOptions['options'][$key] = ['value' => $child['name'], 'text' => $child['label'] = $child['label'] ?: $child['name']];
+            }
         }
 
         return $controlOptions;
