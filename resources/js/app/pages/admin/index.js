80ab6f54146819744f8b10924dbd7a88d8cab718
dante di domenico <dante@didoda.com>
2023-02-22 09:00:02 +0100

    feat: icons in admin pages

--- resources/js/app/pages/admin/index.js
+++ resources/js/app/pages/admin/index.js
@@ -14,6 +14,8 @@ export default {
         Secret: () => import(/* webpackChunkName: "secret" */'app/components/secret/secret'),
         // icons
         IconCopy: () => import(/* webpackChunkName: "icon-copy" */'@carbon/icons-vue/es/copy/16.js'),
+        IconSave: () => import(/* webpackChunkName: "icon-save" */'@carbon/icons-vue/es/save/16.js'),
+        IconTrashCan16: () => import(/* webpackChunkName: "icon-trash-can-16" */'@carbon/icons-vue/es/trash-can/16.js'),
         IconViewFilled: () => import(/* webpackChunkName: "icon-view-filled" */'@carbon/icons-vue/es/view--filled/16.js'),
         IconViewOffFilled: () => import(/* webpackChunkName: "icon-view-off-filled" */'@carbon/icons-vue/es/view--off--filled/16.js'),
     },
@@ -25,7 +27,8 @@ export default {
     methods: {
         remove(e) {
             const message = t`Remove item. Are you sure?`;
-            const form = document.getElementById(e.target.getAttribute('form'));
+            const formId = e.target.closest('button').getAttribute('form');
+            const form = document.getElementById(formId);
             confirm(message, t`yes, proceed`, form.submit.bind(form));
         },
     }
