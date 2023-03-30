4478b873aaa7c78ae54b6ef3111f649ad940a45f
dante di domenico <dante@didoda.com>
2023-03-30 18:51:01 +0200

    fix: permission i18n

--- resources/js/app/components/permission/permission.vue
+++ resources/js/app/components/permission/permission.vue
@@ -25,14 +25,22 @@ export default {
         userRoles: [],
     },
 
+    data() {
+        return {
+            // i18n, @see https://github.com/ttag-org/ttag/issues/201
+            msgInherited: t`Inherited`,
+            msgYourRoles: t`Your roles`,
+        };
+    },
+
     methods: {
         hasPermission() {
             return this.objectRoles.includes(this.role);
         },
         help() {
-            const inherit = this.inherited ? t('inherited') : '';
+            const inherit = this.inherited ? this.msgInherited : '';
 
-            return `${t('Your roles')}: ${this.userRoles.join(',')} ${inherit}`;
+            return `${msgYourRoles}: ${this.userRoles.join(',')} ${inherit}`;
         },
         isLocked() {
             if (this.userRoles.includes('admin')) {
