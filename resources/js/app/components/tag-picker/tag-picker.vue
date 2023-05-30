ed3f0c4d719e105dbc3901fb26d5baf3b8218f69
dante di domenico <dante@didoda.com>
2023-05-30 11:51:39 +0200

    refactor: from js to vue

--- resources/js/app/components/tag-picker/tag-picker.vue
+++ resources/js/app/components/tag-picker/tag-picker.vue
@@ -1,3 +1,33 @@
+<template>
+    <div class="tag-picker">
+        <label v-if="label">{{ label }}</label>
+        <Treeselect
+            placeholder="${t`Search existing tags`}"
+            :options="tagsOptions"
+            :async="true"
+            :clearable="false"
+            :load-options="fetchTags"
+            :disabled="disabled"
+            :disable-branch-nodes="true"
+            :multiple="true"
+            value-format="object"
+            v-model="selectedTags"
+            @select="onAdd"
+            @deselect="onRemove"
+            @input="onChange"
+        />
+        <div class="new-tag" v-show="!searchonly">
+            <label for="new-tag">{{ msgAddNewTag }}</label>
+            <div class="input-container">
+                <input type="text" :form="form" :value="text" id="new-tag" @input="update($event.target.value)" />
+                <button @click.prevent="addNewTag">{{ msgAdd }}</button>
+            </div>
+        </div>
+        <input type="hidden" :form="form" :id="id" name="tags" :value="modifiedTags" />
+        <input type="hidden" :form="form" name="_types[tags]" value="json" />
+    </div>
+</template>
+<script>
 import { Treeselect, ASYNC_SEARCH } from '@riophae/vue-treeselect'
 import { t } from 'ttag';
 import '@riophae/vue-treeselect/dist/vue-treeselect.css'
@@ -15,36 +45,6 @@ export default {
         Treeselect,
     },
 
-    template: `
-        <div class="tag-picker">
-            <label v-if="label"><: label :></label>
-            <Treeselect
-                placeholder="${t`Search existing tags`}"
-                :options="tagsOptions"
-                :async="true"
-                :clearable="false"
-                :load-options="fetchTags"
-                :disabled="disabled"
-                :disable-branch-nodes="true"
-                :multiple="true"
-                value-format="object"
-                v-model="selectedTags"
-                @select="onAdd"
-                @deselect="onRemove"
-                @input="onChange"
-            />
-            <div class="new-tag" v-show="!searchonly">
-                <label for="new-tag">${t`Add new tag`}</label>
-                <div class="input-container">
-                    <input type="text" :form="form" :value="text" id="new-tag" @input="update($event.target.value)" />
-                    <button @click.prevent="addNewTag">${t`Add`}</button>
-                </div>
-            </div>
-            <input type="hidden" :form="form" :id="id" name="tags" :value="modifiedTags" />
-            <input type="hidden" :form="form" name="_types[tags]" value="json" />
-        </div>
-    `,
-
     props: {
         id: String,
         disabled: Boolean,
@@ -60,6 +60,8 @@ export default {
             modifiedTags: null,
             tagsOptions: [],
             text: '',
+            msgAdd: t`Add`,
+            msgAddNewTag: t`Add new tag`,
         };
     },
 
@@ -123,3 +125,4 @@ export default {
         },
     },
 }
+</script>
