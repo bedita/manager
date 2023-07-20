4de3ec0f44c51b75b0145e43d8557af4d7e59857
dante di domenico <dante@didoda.com>
2023-07-20 17:06:48 +0200

    fix: object-property

--- resources/js/app/components/object-property/object-property.vue
+++ resources/js/app/components/object-property/object-property.vue
@@ -8,35 +8,42 @@
                         <Icon icon="carbon:information"></Icon>
                     </i>
                 </span>
-                <div class="grid">
-                    <span class="column-header">{{ msgLabel }}</span>
-                    <span>{{ prop.attributes.label || '-' }}</span>
-
-                    <span class="column-header">{{ msgType }}</span>
-                    <span class="property-val">{{ prop.attributes.property_type_name }}</span>
-
-                    <span class="column-header">{{ msgReadonly }}</span>
-                    <span class="property-val">{{ readonly }}</span>
-
-                    <span class="column-header">{{ msgHidden }}</span>
-                    <span>
-                        <select v-model="hidden" @change="updateHidden()">
-                            <option value="true">{{ msgYes }}</option>
-                            <option value="false">{{ msgNo }}</option>
-                        </select>
-                    </span>
-
-                    <span class="column-header">{{ msgTranslatable }}</span>
-                    <span>
-                        <select v-model="translatable" @change="updateTranslationRules()">
-                            <option value="true">{{ msgYes }}</option>
-                            <option value="false">{{ msgNo }}</option>
-                        </select>
-                    </span>
-
-                    <span v-if="type === 'inherited'" class="column-header">{{ msgInheritedFrom }}</span>
-                    <span v-if="type === 'inherited'">{{ prop.attributes.object_type_name }}</span>
-                </div>
+                <table>
+                    <tr>
+                        <td class="column-header">{{ msgLabel }}</td>
+                        <td>{{ prop.attributes.label }}</td>
+                    </tr>
+                    <tr>
+                        <td class="column-header">{{ msgType }}</td>
+                        <td class="property-val">{{ prop.attributes.property_type_name }}</td>
+                    </tr>
+                    <tr>
+                        <td class="column-header">{{ msgReadonly }}</td>
+                        <td class="property-val">{{ readonly }}</td>
+                    </tr>
+                    <tr>
+                        <td class="column-header">{{ msgHidden }}</td>
+                        <td>
+                            <select v-model="hidden" @change="updateHidden()">
+                                <option :value="true">{{ msgYes }}</option>
+                                <option :value="false">{{ msgNo }}</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="column-header">{{ msgTranslatable }}</td>
+                        <td>
+                            <select v-model="translatable" @change="updateTranslationRules()">
+                                <option value="true">{{ msgYes }}</option>
+                                <option value="false">{{ msgNo }}</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr v-if="type === 'inherited'">
+                        <td v-if="type === 'inherited'" class="column-header">{{ msgInheritedFrom }}</td>
+                        <td v-if="type === 'inherited'">{{ prop.attributes.object_type_name }}</td>
+                    </tr>
+                </table>
             </div>
             <div v-if="!nobuttonsfor.includes(prop.attributes.name)" class="column is-narrow">
                 <div class="buttons-container">
@@ -91,16 +98,16 @@ export default {
     computed: {
         boxClass() {
             if (this.hidden === true) {
-                return 'box pb-05 has-background-gray-600 has-text-white';
+                return 'container box pb-05 has-background-gray-600 has-text-white';
             }
             if (this.type === 'custom') {
-                return 'box pb-05 has-background-info has-text-white';
+                return 'container box pb-05 has-background-info has-text-white';
             }
             if (this.type === 'inherited') {
-                return 'box pb-05 has-background-gray-800 has-text-white';
+                return 'container box pb-05 has-background-gray-800 has-text-white';
             }
 
-            return 'box pb-05 has-background-black has-text-white';
+            return 'container box pb-05 has-background-black has-text-white';
         },
         tagClass() {
             if (this.type === 'custom') {
@@ -118,7 +125,7 @@ export default {
         updateHidden() {
             let hiddenProperties = JSON.parse(document.getElementById('hidden').value || '[]') || [];
             const index = hiddenProperties.indexOf(this.prop.attributes.name);
-            if (index >= 0 && this.hidden === 'false') {
+            if (index >= 0 && this.hidden === false) {
                 // remove property from hidden properties
                 hiddenProperties.splice(index, 1);
                 const newVal = JSON.stringify(hiddenProperties);
@@ -126,7 +133,7 @@ export default {
 
                 return;
             }
-            if (index < 0 && this.hidden === 'true') {
+            if (index < 0 && this.hidden === true) {
                 // add property to hidden properties
                 hiddenProperties.push(this.prop.attributes.name);
                 hiddenProperties.sort();
@@ -181,8 +188,7 @@ export default {
 }
 </script>
 <style>
-div.box {
-    min-width: 350px;
+div.container {
     padding: 0.5rem;
     margin: 0.5rem 0;
     border-radius: 25px;
@@ -193,12 +199,6 @@ div.column > p {
     margin: 0.5rem 0;
     border-bottom: dashed 1px gray;
 }
-div.column > div.grid {
-    display: grid;
-    grid-template-columns: 100px 1fr;
-    border-top: 1px dotted black;
-    border-right: 1px dotted black;
-}
 div.column > div > span {
     padding: 4px 8px;
     border-left: 1px dotted black;
@@ -206,14 +206,18 @@ div.column > div > span {
     white-space: normal;
     vertical-align: middle;
 }
-div.column > div > span.column-header {
-    color: yellow;
-    text-align: left;
-}
 div.column > div > span > a:hover {
     text-decoration: underline;
 }
-div.column > div > span.property-val {
+div.column > div.rows > div {
+    border-bottom: solid 1px gray;
+    padding: 0.5rem 0;
+}
+td.column-header {
+    color: yellow;
+    text-align: left;
+}
+td.property-val {
     font-family: monospace;
 }
 </style>
