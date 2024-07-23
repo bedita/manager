0d0e5421be18a78d527d7e2944de5090925b6063
dante di domenico <dante@didoda.com>
2024-07-23 15:31:14 +0200

    fix: tests

--- tests/TestCase/Controller/CategoriesControllerTest.php
+++ tests/TestCase/Controller/CategoriesControllerTest.php
@@ -58,7 +58,7 @@ class CategoriesControllerTest extends TestCase
                 'object_type' => 'documents',
             ],
         ]));
-        $this->CategoriesController->viewBuilder()->setVar('project', ['version' => '5.27.0']);
+        $this->CategoriesController->viewBuilder()->setVar('project', ['version' => '5.26.0']);
         $result = $this->CategoriesController->index();
 
         // verify response status code and type
