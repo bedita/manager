5174c2529f98c3968ee3df22f04e564aa91a7cd7
dante di domenico <dante@didoda.com>
2025-02-17 11:39:51 +0100

    fix: border

--- resources/js/app/components/fast-create/fast-create.vue
+++ resources/js/app/components/fast-create/fast-create.vue
@@ -269,7 +269,7 @@ div.fast-create {
     flex-direction: column;
     margin: 1rem 0;
     padding: 1rem 0;
-    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='white' stroke-width='3' stroke-dasharray='6' stroke-dashoffset='29' stroke-linecap='butt'/%3e%3c/svg%3e");
+    border: solid 1px white;
 }
 div.fast-create > div {
     margin: 0.5rem 1rem 0.5rem 1rem;
