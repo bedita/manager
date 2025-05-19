06f8e881fb0d1a9f4a078a22f972b9ae2e395528
dante di domenico <dante@didoda.com>
2025-05-19 11:00:22 +0200

    feat: add related by id

---
+++ resources/js/app/components/add-related-by-id/add-related-by-id.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="add-related-by-id ml-1">
+        <input
+            type="text"
+            placeholder="ID or uname"
+            v-model="searchId"
+        >
+        <button
+            class="button button-primary"
+            :disabled="!searchId"
+            @click.prevent="add"
+        >
+            <app-icon icon="carbon:add" />
+            <span class="ml-05">{{ msgAdd }}</span>
+        </button>
+    </div>
+</template>
+<script>
+import { t } from 'ttag';
+
+export default {
+    name: 'AddRelatedById',
+    props: {
+        objectTypes: {
+            type: Array,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            cache: {},
+            msgAdd: t`Add`,
+            msgAllowedTypes: t`Allowed types`,
+            msgNotFound: t`Not found`,
+            searchId: '',
+        };
+    },
+    methods: {
+        async add() {
+            try {
+                const obj = await this.fetchObject(this.searchId);
+                this.$emit('found', obj);
+            } catch (error) {
+                BEDITA.error(`${this.msgNotFound}. ${this.msgAllowedTypes}: ${this.objectTypes.join(', ')}`);
+            }
+        },
+        async fetchObject(id) {
+            if (this.cache[id]) {
+                return this.cache[id];
+            }
+            const baseUrl = new URL(BEDITA.base).pathname;
+            const response = await fetch(`${baseUrl}resources/get/${id}`, {
+                credentials: 'same-origin',
+                headers: {
+                    accept: 'application/json',
+                }
+            });
+            const responseJson = await response.json();
+            const data = responseJson?.data || {};
+            if (!data) {
+                throw new Error('Object not found');
+            }
+            if (!this.objectTypes.includes(data?.type)) {
+                throw new Error('Invalid object type');
+            }
+            this.cache[id] = data;
+
+            return this.cache[id];
+        },
+    },
+};
+</script>
+<style scoped>
+div.add-related-by-id {
+    display: flex;
+    align-items: center;
+    max-width: 200px;
+}
+div.add-related-by-id button {
+    margin-left: 0.25rem;
+}
+</style>
