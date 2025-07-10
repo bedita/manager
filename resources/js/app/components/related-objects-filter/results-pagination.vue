87d9e6f67a04e82312dfa731a832a1613848129c
dante di domenico <dante@didoda.com>
2025-07-10 09:28:21 +0200

    fix: better pagination

--- resources/js/app/components/related-objects-filter/results-pagination.vue
+++ resources/js/app/components/related-objects-filter/results-pagination.vue
@@ -27,15 +27,35 @@
                     v-if="pages > 1"
                 >
                     <div>
-                        <button
-                            v-for="page in pages"
-                            :key="page"
-                            class="button is-width-auto has-text-size-smallest"
-                            :class="{ 'current-page': page === (filter?.page || 1), 'button-outlined': page !== (filter?.page || 1) }"
-                            @click.prevent="updatePage(page)"
-                        >
-                            {{ page }}
-                        </button>
+                        <template v-for="page in pages">
+                            <input
+                                size="2"
+                                class="ml-05 mr-05"
+                                type="number"
+                                min="1"
+                                :max="pages"
+                                :key="`input-${page}`"
+                                v-model="changePage"
+                                @keyup.enter="updatePage(changePage)"
+                                @change="updatePage(changePage)"
+                                v-if="page == changePage"
+                            >
+                            <button
+                                :key="page"
+                                class="button is-width-auto has-text-size-smallest"
+                                :class="{ 'current-page': page === (filter?.page || 1), 'button-outlined': page !== (filter?.page || 1) }"
+                                @click.prevent="updatePage(page)"
+                                v-if="visiblePages.includes(page) && page !== changePage"
+                            >
+                                {{ page }}
+                            </button>
+                            <template v-if="!visiblePages.includes(page) && (page === changePage - 2 || page === changePage + 2)">
+                                <span
+                                    :key="page"
+                                    class="pages-delimiter"
+                                />
+                            </template>
+                        </template>
                     </div>
                 </div>
             </nav>
@@ -64,6 +84,7 @@ export default {
             changePage: null,
             changeSize: null,
             pages: 1,
+            visiblePages: [],
         };
     },
     mounted() {
@@ -71,6 +92,25 @@ export default {
             this.changePage = this.filter?.page || this.options.page || 1,
             this.changeSize = this.filter?.pageSize || this.options.pageSize || 10,
             this.pages = Math.ceil(this.count / this.changeSize);
+            let visiblePages = [];
+            if (this.pages <= 5) {
+                visiblePages = Array.from({ length: this.pages }, (_, i) => i + 1);
+                visiblePages = [...new Set(visiblePages)];
+                visiblePages.sort();
+                this.visiblePages = visiblePages;
+                return;
+            }
+            // visible pages should be the first, the last, current, the previous and the next page
+            visiblePages = [
+                1,
+                this.pages,
+                this.changePage - 1,
+                this.changePage,
+                this.changePage + 1
+            ].filter(page => page > 0 && page <= this.pages);
+            visiblePages = [...new Set(visiblePages)];
+            visiblePages.sort();
+            this.visiblePages = visiblePages;
         });
     },
     methods: {
@@ -81,7 +121,7 @@ export default {
             });
         },
         updatePage(page) {
-            this.changePage = page;
+            this.changePage = parseInt(page);
             this.update();
         },
     },
