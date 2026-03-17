46cb375075d3a5816ba8d10b26a0a55f4815aa41
dante di domenico <dante@didoda.com>
2026-03-17 08:37:11 +0100

    fix: show all

--- resources/js/app/components/admin-job-list/admin-job-list.vue
+++ resources/js/app/components/admin-job-list/admin-job-list.vue
@@ -2,110 +2,108 @@
     <div class="columns">
         <div class="column">
             <section class="fieldset">
-                <details>
-                    <summary @click="toggleOpen" :open="isOpen">{{ tr(service) }}</summary>
-                    <div>
-                        <div
-                            class="is-loading-spinner mt-05"
-                            v-if="loading"
-                        />
-                        <div
-                            class="tab-container"
-                            v-if="!loading && jobs?.length > 0"
-                        >
-                            <div>
-                                Page
-                                <template v-if="pagination.page > 1">
-                                    <a @click="updateJobs(pagination.page - 1)"><app-icon icon="carbon:chevron-left" /></a>
-                                </template>
-                                {{ pagination.page }} / {{ pagination.page_count }}
-                                <template v-if="pagination.page < pagination.page_count">
-                                    <a @click="updateJobs(pagination.page + 1)"><app-icon icon="carbon:chevron-right" /></a>
-                                </template>
-                            </div>
-                            <div id="list-jobs" :class="{ 'file-column': columnFile }">
-                                <div class="table-header">
-                                    Job
-                                </div>
-                                <div class="table-header" v-if="columnFile">
-                                    {{ msgFileName }}
-                                </div>
-                                <div class="table-header">
-                                    {{ msgServiceName }}
-                                </div>
-                                <div class="table-header">
-                                    {{ msgScheduledFrom }}
-                                </div>
-                                <div class="table-header">
-                                    {{ msgCompletedOn }}
-                                </div>
-                                <div class="table-header">
-                                    {{ msgStatus }}
-                                </div>
-                                <div class="table-header" />
-                                <template v-for="job in jobs">
-                                    <div :class="job.meta.status">
-                                        {{ fmt(job.meta.created) }}
-                                        <br />
-                                        {{ job.id }}
-                                    </div>
-                                    <div :class="job.meta.status" v-if="columnFile">
-                                        {{ job.attributes.payload && job.attributes.payload.filename }}
-                                    </div>
-                                    <div :class="job.meta.status">
-                                        {{ job.attributes.service }}
-                                    </div>
-                                    <div :class="job.meta.status">
-                                        {{ fmt(job.attributes.scheduled_from) }}
-                                    </div>
-                                    <div :class="job.meta.status">
-                                        {{ fmt(job.meta.completed) }}
-                                    </div>
-                                    <div :class="job.meta.status">
-                                        {{ job.meta.status }}
-                                    </div>
-                                    <div :class="job.meta.status">
-                                        <a :class="showPayloadId != job.id ? 'icon-plus' : 'icon-minus'"
-                                            @click.prevent="togglePayload(job.id)"
-                                        />
-                                    </div>
-                                    <div
-                                        class="job-payload" :class="{ 'job-payload-file': columnFile }"
-                                        v-show="showPayloadId == job.id"
-                                    >
-                                        <h3>Payload</h3>
-                                        <div :id="`container-payload-${job.id}`" />
-                                        <json-editor
-                                            :options="jsonEditorOptions"
-                                            :target="`container-payload-${job.id}`"
-                                            :text="JSON.stringify(job.attributes.payload)"
-                                        />
-
-                                        <h3 v-if="job.attributes.results">
-                                            Results
-                                        </h3>
-                                        <div
-                                            :id="`container-results-${job.id}`"
-                                            v-if="job.attributes.results"
-                                        />
-                                        <json-editor
-                                            :options="jsonEditorOptions"
-                                            :target="`container-results-${job.id}`"
-                                            :text="JSON.stringify(job.attributes.results)"
-                                            v-if="job.attributes.results"
-                                        />
-                                    </div>
-                                </template>
-                            </div>
+                <h4 v-if="service !== 'all'">{{ tr(service) }}</h4>
+                <div>
+                    <div
+                        class="is-loading-spinner mt-05"
+                        v-if="loading"
+                    />
+                    <div
+                        class="tab-container"
+                        v-if="!loading && jobs?.length > 0"
+                    >
+                        <div>
+                            Page
+                            <template v-if="pagination.page > 1">
+                                <a @click="updateJobs(pagination.page - 1)"><app-icon icon="carbon:chevron-left" /></a>
+                            </template>
+                            {{ pagination.page }} / {{ pagination.page_count }}
+                            <template v-if="pagination.page < pagination.page_count">
+                                <a @click="updateJobs(pagination.page + 1)"><app-icon icon="carbon:chevron-right" /></a>
+                            </template>
                         </div>
-                        <div
-                            class="mt-05"
-                            v-if="!loading && jobs.length === 0"
-                        >
-                            {{ msgNoJobs }}
+                        <div id="list-jobs" :class="{ 'file-column': columnFile }">
+                            <div class="table-header">
+                                Job
+                            </div>
+                            <div class="table-header" v-if="columnFile">
+                                {{ msgFileName }}
+                            </div>
+                            <div class="table-header">
+                                {{ msgServiceName }}
+                            </div>
+                            <div class="table-header">
+                                {{ msgScheduledFrom }}
+                            </div>
+                            <div class="table-header">
+                                {{ msgCompletedOn }}
+                            </div>
+                            <div class="table-header">
+                                {{ msgStatus }}
+                            </div>
+                            <div class="table-header" />
+                            <template v-for="job in jobs">
+                                <div :class="job.meta.status">
+                                    {{ fmt(job.meta.created) }}
+                                    <br />
+                                    {{ job.id }}
+                                </div>
+                                <div :class="job.meta.status" v-if="columnFile">
+                                    {{ job.attributes.payload && job.attributes.payload.filename }}
+                                </div>
+                                <div :class="job.meta.status">
+                                    {{ job.attributes.service }}
+                                </div>
+                                <div :class="job.meta.status">
+                                    {{ fmt(job.attributes.scheduled_from) }}
+                                </div>
+                                <div :class="job.meta.status">
+                                    {{ fmt(job.meta.completed) }}
+                                </div>
+                                <div :class="job.meta.status">
+                                    {{ job.meta.status }}
+                                </div>
+                                <div :class="job.meta.status">
+                                    <a :class="showPayloadId != job.id ? 'icon-plus' : 'icon-minus'"
+                                        @click.prevent="togglePayload(job.id)"
+                                    />
+                                </div>
+                                <div
+                                    class="job-payload" :class="{ 'job-payload-file': columnFile }"
+                                    v-show="showPayloadId == job.id"
+                                >
+                                    <h3>Payload</h3>
+                                    <div :id="`container-payload-${job.id}`" />
+                                    <json-editor
+                                        :options="jsonEditorOptions"
+                                        :target="`container-payload-${job.id}`"
+                                        :text="JSON.stringify(job.attributes.payload)"
+                                    />
+
+                                    <h3 v-if="job.attributes.results">
+                                        Results
+                                    </h3>
+                                    <div
+                                        :id="`container-results-${job.id}`"
+                                        v-if="job.attributes.results"
+                                    />
+                                    <json-editor
+                                        :options="jsonEditorOptions"
+                                        :target="`container-results-${job.id}`"
+                                        :text="JSON.stringify(job.attributes.results)"
+                                        v-if="job.attributes.results"
+                                    />
+                                </div>
+                            </template>
                         </div>
                     </div>
-                </details>
+                    <div
+                        class="mt-05"
+                        v-if="!loading && jobs.length === 0"
+                    >
+                        {{ msgNoJobs }}
+                    </div>
+                </div>
             </section>
         </div>
     </div>
@@ -149,8 +147,11 @@ export default {
         };
     },
 
-    mounted() {
+    async mounted() {
         this.columnFile = !['all', 'credentials_change', 'mail', 'signup', 'thumbnail'].includes(this.service);
+        this.$nextTick(async () => {
+            this.toggleOpen();
+        });
     },
 
     methods: {
